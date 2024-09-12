bed91e2c4f5dccc77ed71c6756c834ada2985c97
ChristopherTs <christophertshoma0045@gmail.com>
2024-09-12 21:36:17 +0200

    "Refactor action buttons in ProductGrid component to use relative positioning and add tooltips with button text"

--- app/components/ProductGrid.js
+++ app/components/ProductGrid.js
@@ -30,56 +30,78 @@ export default function ProductGrid({ products }) {
 
             {/* Action Buttons */}
             <div className="flex justify-around mt-4">
-              {/* Add to Cart Button */}
-              <button onClick={() => console.log("Added to cart:", product)}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 text-green-500 hover:text-green-700"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M3 3h2l3.6 7.59-1.35 2.44C7.07 13.84 7 14.16 7 14.5 7 15.33 7.67 16 8.5 16h9c.63 0 1.18-.39 1.41-.97l3.38-8.51C22.64 6.02 22.41 5 21.69 5H6.21l-.94-2H3zm5 13c-.83 0-1.5.67-1.5 1.5S7.17 19 8 19s1.5-.67 1.5-1.5S8.83 16 8 16zm7 0c-.83 0-1.5.67-1.5 1.5S14.17 19 15 19s1.5-.67 1.5-1.5S15.83 16 15 16z" />
-                </svg>
-              </button>
+  {/* Add to Cart Button */}
+  <div className="relative group">
+    <button
+      onClick={() => console.log("Added to cart:", product)}
+      className="bg-gray-200 p-3 rounded-full shadow hover:bg-gray-300"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 text-green-500 group-hover:text-green-700"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+      >
+        <path d="M3 3h2l3.6 7.59-1.35 2.44C7.07 13.84 7 14.16 7 14.5 7 15.33 7.67 16 8.5 16h9c.63 0 1.18-.39 1.41-.97l3.38-8.51C22.64 6.02 22.41 5 21.69 5H6.21l-.94-2H3zm5 13c-.83 0-1.5.67-1.5 1.5S7.17 19 8 19s1.5-.67 1.5-1.5S8.83 16 8 16zm7 0c-.83 0-1.5.67-1.5 1.5S14.17 19 15 19s1.5-.67 1.5-1.5S15.83 16 15 16z" />
+      </svg>
+    </button>
+    <span className="absolute bottom-0 transform translate-y-10 group-hover:translate-y-0 opacity-0 group-hover:opacity-100 transition-all duration-300 text-xs text-gray-700">
+      Add to Cart
+    </span>
+  </div>
 
-              {/* Add to Wishlist Button */}
-              <button
-                onClick={() => console.log("Added to wishlist:", product)}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 text-red-500 hover:text-red-700"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364l-1.318 1.318-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  />
-                </svg>
-              </button>
+  {/* Add to Wishlist Button */}
+  <div className="relative group">
+    <button
+      onClick={() => console.log("Added to wishlist:", product)}
+      className="bg-gray-200 p-3 rounded-full shadow hover:bg-gray-300"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 text-red-500 group-hover:text-red-700"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364l-1.318 1.318-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+        />
+      </svg>
+    </button>
+    <span className="absolute bottom-0 transform translate-y-10 group-hover:translate-y-0 opacity-0 group-hover:opacity-100 transition-all duration-300 text-xs text-gray-700">
+      Wishlist
+    </span>
+  </div>
 
-              {/* View Product Button */}
-              <Link href={`/products/${product.id}`}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 text-blue-500 hover:text-blue-700"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0zm1.5 0a6.5 6.5 0 01-13 0 6.5 6.5 0 0113 0z"
-                  />
-                </svg>
-              </Link>
-            </div>
+  {/* View Product Button */}
+  <div className="relative group">
+    <Link href={`/products/${product.id}`}>
+      <button className="bg-gray-200 p-3 rounded-full shadow hover:bg-gray-300">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-blue-500 group-hover:text-blue-700"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M5 12h14v6H5zM4 6h16v4H4z"
+          />
+        </svg>
+      </button>
+    </Link>
+    <span className="absolute bottom-0 transform translate-y-10 group-hover:translate-y-0 opacity-0 group-hover:opacity-100 transition-all duration-300 text-xs text-gray-700">
+      View Product
+    </span>
+  </div>
+</div>
+
           </div>
         </div>
       ))}
